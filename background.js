f0f7efe14993e39052e135c975804f8ccd3d56d0
proEndreeper <devon.schneider+gpg@outlook.com>
2018-10-02 17:21:07 -0600

    Reverted to general cases for domains, and added more error checking.

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
 // Simple extension to redirect all requests to RS Wikia to RS Wiki
 
-const RSWIKIA_REGEX = /^(http|https):\/\/(oldschoolrunescape|runescape|2007\.runescape)\.wikia\.com(.*)$/i;
+const RSWIKIA_REGEX = /^(http|https):\/\/([^\.]+)\.wikia\.com(.*)$/i;
 
 const REDIRECTS = {
   "oldschoolrunescape":"oldschool.runescape.wiki",
-  "2007.runescape":"oldschool.runescape.wiki",
   "runescape":"runescape.wiki"
 };
 
@@ -15,17 +14,19 @@ function splitURL(url){
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
     let splitUrl = splitURL(info.url);
-    if(splitUrl===null) return;
-    let redirectUrl = "https://"+REDIRECTS[splitUrl[2]]+splitUrl[3].replace(/^\/wiki\//i,"/w/");
-    console.log("RSWiki intercepted: " + info.url + "\nRedirecting to "+redirectUrl);
+    let newDomain = REDIRECTS[splitUrl[2]];
+    // If domain isn't subdomain of wikia.com, ignore, also if it's not in the redirect filter
+    if(splitUrl===null || newDomain===undefined) return;
+    // Generate new url
+    let redirectUrl = "https://"+newDomain+splitUrl[3].replace(/^\/wiki\//i,"/w/");
+    console.log("RSWikia intercepted: " + info.url + "\nRedirecting to "+redirectUrl);
     // Redirect the old wikia request to new wiki	
-	return {redirectUrl:redirectUrl};
+    return {redirectUrl:redirectUrl};
   },
   // filters
   {
     urls: [
       "*://oldschoolrunescape.wikia.com/*",
-      "*://2007.runescape.wikia.com/*",
       "*://runescape.wikia.com/*"
     ]
   },
