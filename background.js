e74a37c35523061d14e95dc9b6a8cf8cb429a153
proEndreeper <devon.schneider+gpg@outlook.com>
2018-10-04 20:30:20 -0600

    Implemented a way to disable extension if you don't want to be redirected.
    Included source files for the power button used in the extension popup.

--- background.js
+++ background.js
@@ -1,34 +1,68 @@
 // Simple extension to redirect all requests to RS Wikia to RS Wiki
+(function(){
+  'use strict';
+  var isPluginDisabled = false;
 
-const RSWIKIA_REGEX = /^(http|https):\/\/([^\.]+)\.wikia\.com(.*)$/i;
+  let storage = window.storage || chrome.storage;
 
-const REDIRECTS = {
-  "oldschoolrunescape":"oldschool.runescape.wiki",
-  "runescape":"runescape.wiki"
-};
+  const RSWIKIA_REGEX = /^(http|https):\/\/([^\.]+)\.wikia\.com(.*)$/i;
 
-function splitURL(url){
-  return RSWIKIA_REGEX.exec(url);
-}
+  const REDIRECTS = {
+    "oldschoolrunescape":"oldschool.runescape.wiki",
+    "runescape":"runescape.wiki"
+  };
 
-chrome.webRequest.onBeforeRequest.addListener(
-  function(info) {
-    let splitUrl = splitURL(info.url);
-    let newDomain = REDIRECTS[splitUrl[2]];
-    // If domain isn't subdomain of wikia.com, ignore, also if it's not in the redirect filter
-    if(splitUrl===null || newDomain===undefined) return;
-    // Generate new url
-    let redirectUrl = "https://"+newDomain+splitUrl[3].replace(/^\/wiki\//i,"/w/");
-    console.log("RSWikia intercepted: " + info.url + "\nRedirecting to "+redirectUrl);
-    // Redirect the old wikia request to new wiki	
-    return {redirectUrl:redirectUrl};
-  },
-  // filters
-  {
-    urls: [
-      "*://oldschoolrunescape.wikia.com/*",
-      "*://runescape.wikia.com/*"
-    ]
-  },
-  // extraInfoSpec
-  ["blocking"]);
+  function splitURL(url){
+    return RSWIKIA_REGEX.exec(url);
+  }
+
+  chrome.webRequest.onBeforeRequest.addListener(
+    function(info) {
+      let splitUrl = splitURL(info.url);
+      let newDomain = REDIRECTS[splitUrl[2]];
+      // If domain isn't subdomain of wikia.com, ignore, also if it's not in the redirect filter
+      if(splitUrl===null || newDomain===undefined) return;
+      // Check if plugin is disabled, if so put a message logged
+      if(isPluginDisabled) {
+        console.log("RSWikia intercepted, ignoring because plugin is disabled.");
+        return;
+      }
+      // Generate new url
+      let redirectUrl = "https://"+newDomain+splitUrl[3].replace(/^\/wiki\//i,"/w/");
+      console.log("RSWikia intercepted: " + info.url + "\nRedirecting to "+redirectUrl);
+      // Redirect the old wikia request to new wiki	
+      return {redirectUrl:redirectUrl};
+    },
+    // filters
+    {
+      urls: [
+        "*://oldschoolrunescape.wikia.com/*",
+        "*://runescape.wikia.com/*"
+      ]
+    },
+    // extraInfoSpec
+    ["blocking"]);
+
+  function updateIcon(){
+    (chrome.browserAction).setIcon({  path: isPluginDisabled?"icon32_black.png":"icon32.png"  });
+  }
+
+  storage.local.get(['isDisabled'],(result)=>{
+      if(result===undefined) {
+        result={isDisabled:false};
+      }
+      isPluginDisabled=result.isDisabled;
+      updateIcon();
+  });
+
+  storage.onChanged.addListener(
+      function(changes, areaName) {
+        // If isDisabled changed, update isPluginDisabled
+        if(changes["isDisabled"]!==undefined && changes["isDisabled"].newValue!=changes["isDisabled"].oldValue) {
+          console.log("RS Wiki Redirector is now "+(changes["isDisabled"].newValue?"disabled":"enabled")+".");
+          isPluginDisabled=changes["isDisabled"].newValue;
+          updateIcon();
+        }
+      }
+    );
+})();
---
+++ popup.js
@@ -0,0 +1,38 @@
+(function(){
+	let storage = window.storage || chrome.storage;
+	let stateImage = document.getElementById("addonStateImage");
+
+	if(stateImage==null)
+	{
+		console.error("Something went wrong!");
+		return;
+	}
+	function toggle()
+	{
+			let prom = storage.local.set({isDisabled:!currentState});
+			if(window.Promise && prom instanceof Promise) {
+				prom.reject(()=>{});
+			}
+			update();
+	}
+	stateImage.addEventListener("click",toggle)
+	let stateDesc = document.getElementById("addonStateDescriptor");
+	if(stateDesc==null)
+	{
+		console.error("Something went wrong!");
+		return;
+	}
+	window.update = function() {
+		storage.local.get(['isDisabled'], function(result){
+			if(result===undefined) {
+				result={isDisabled:false};
+			}
+			window.currentState = result.isDisabled==true;
+			stateImage.style.backgroundImage = "url('powerbutton_"+(result.isDisabled?"off":"on")+".png')";
+			stateDesc.innerHTML = result.isDisabled?"disabled":"enabled";
+			stateDesc.style.color = result.isDisabled?"#7C1D1D":"#217A3D";
+			(chrome.browserAction).setIcon({path:result.isDisabled?"icon32_black.png":"icon32.png"});
+		});
+	}
+	update();
+})();
