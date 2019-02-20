89d76685c791437f48d471fd28b6c18de2d32f5f
Richard Honor <richard.honor@infosum.com>
2019-02-20 22:07:17 +0000

    Add searchFilter to hide RS wikia result, minor refactor of background.js

--- background.js
+++ background.js
@@ -1,36 +1,30 @@
 // Simple extension to redirect all requests to RS Wikia to RS Wiki
 (function(){
   'use strict';
-  var isPluginDisabled = false;
-
+  let isPluginDisabled = false;
   let storage = window.storage || chrome.storage;
 
-  const RSWIKIA_REGEX = /^(http|https):\/\/([^\.]+)\.(wikia|fandom)\.com(.*)$/i;
-
-  const REDIRECTS = {
-    "oldschoolrunescape":"oldschool.runescape.wiki",
-    "runescape":"runescape.wiki"
-  };
-
-  function splitURL(url){
-    return RSWIKIA_REGEX.exec(url);
-  }
+  const RSWIKIA_REGEX = /^(runescape|oldschoolrunescape)\.(wikia|fandom)\.com$/i;
 
   chrome.webRequest.onBeforeRequest.addListener(
     function(info) {
-      let splitUrl = splitURL(info.url);
-      let newDomain = REDIRECTS[splitUrl[2]];
-      // If domain isn't subdomain of wikia.com, ignore, also if it's not in the redirect filter
-      if(splitUrl===null || newDomain===undefined) return;
-      // Check if plugin is disabled, if so put a message logged
       if(isPluginDisabled) {
         console.log("RSWikia intercepted, ignoring because plugin is disabled.");
         return;
       }
+
+      const url = new URL(info.url);
+
+      const isWikia = RSWIKIA_REGEX.test(url.host); // is this necessary? we already filter by URL
+      // If domain isn't subdomain of wikia.com, ignore, also if it's not in the redirect filter
+      if (!isWikia) return;
+
       // Generate new url
-      let redirectUrl = "https://"+newDomain+splitUrl[4].replace(/^\/wiki\//i,"/w/");
-      console.log("RSWikia intercepted: " + info.url + "\nRedirecting to "+redirectUrl);
-      // Redirect the old wikia request to new wiki	
+
+      const host = url.host.includes('oldschool') ? 'oldschool.runescape' : 'runescape'
+      const redirectUrl = `https://${host}.wiki${url.pathname.replace(/^\/wiki\//i,"/w/")}`;
+      console.log(`RSWikia intercepted:  ${info.url}\nRedirecting to ${redirectUrl}`);
+      // Redirect the old wikia request to new wiki
       return {redirectUrl:redirectUrl};
     },
     // filters
@@ -39,21 +33,18 @@
         "*://oldschoolrunescape.wikia.com/*",
         "*://runescape.wikia.com/*",
         "*://oldschoolrunescape.fandom.com/*",
-        "*://runescape.fandom.com/*"
+        "*://runescape.fandom.com/*",
       ]
     },
     // extraInfoSpec
     ["blocking"]);
 
   function updateIcon(){
-    chrome.browserAction.setIcon({  path: isPluginDisabled?"icon32_black.png":"icon32.png"  });
+    chrome.browserAction.setIcon({ path: isPluginDisabled?"icon32_black.png":"icon32.png"  });
   }
 
   storage.local.get(['isDisabled'],(result)=>{
-      if(result===undefined) {
-        result={isDisabled:false};
-      }
-      isPluginDisabled=result.isDisabled;
+      isPluginDisabled= result ? result.isDisabled : false;
       updateIcon();
   });
 
@@ -61,7 +52,7 @@
       function(changes, areaName) {
         // If isDisabled changed, update isPluginDisabled
         if(changes["isDisabled"]!==undefined && changes["isDisabled"].newValue!=changes["isDisabled"].oldValue) {
-          console.log("RS Wiki Redirector is now "+(changes["isDisabled"].newValue?"disabled":"enabled")+".");
+          console.log(`RS Wiki Redirector is now ${changes["isDisabled"].newValue?'disabled':'enabled'}`);
           isPluginDisabled=changes["isDisabled"].newValue;
           updateIcon();
         }
--- popup.js
+++ popup.js
@@ -1,36 +1,40 @@
-(function(){
+(function () {
 	let storage = window.storage || chrome.storage;
 	let stateImage = document.getElementById("addonStateImage");
 
-	if(stateImage==null)
-	{
+	if (stateImage === null) {
 		console.error("Something went wrong!");
 		return;
 	}
-	function toggle()
-	{
-			let prom = storage.local.set({isDisabled:!currentState});
-			if(window.Promise && prom instanceof Promise) {
-				prom.reject(()=>{});
-			}
-			update();
+	function toggle() {
+		let prom = storage.local.set({ isDisabled: !currentState });
+		if (prom instanceof Promise) {
+			prom.reject();
+		}
+		update();
 	}
-	stateImage.addEventListener("click",toggle)
+	stateImage.addEventListener("click", toggle)
 	let stateDesc = document.getElementById("addonStateDescriptor");
-	if(stateDesc==null)
-	{
+	if (stateDesc === null) {
 		console.error("Something went wrong!");
 		return;
 	}
-	window.update = function() {
-		storage.local.get(['isDisabled'], function(result){
-			if(result===undefined) {
-				result={isDisabled:false};
+
+	const hideWikiaCheck = document.querySelector("#hideWikiaCheck")
+	hideWikiaCheck.addEventListener("click", (e) => {
+		storage.local.set({ hideWikia: e.target.checked });
+	});
+
+	window.update = function () {
+		storage.local.get(['isDisabled', 'hideWikia'], function (result) {
+			if (result === undefined) {
+				result = { isDisabled: false, hideWikia: true };
 			}
-			window.currentState = result.isDisabled==true;
-			stateImage.style.backgroundImage = "url('powerbutton_"+(result.isDisabled?"off":"on")+".png')";
-			stateDesc.innerHTML = result.isDisabled?"disabled":"enabled";
-			stateDesc.style.color = result.isDisabled?"#7C1D1D":"#217A3D";
+			window.currentState = result.isDisabled === true;
+			stateImage.style.backgroundImage = "url('powerbutton_" + (result.isDisabled ? "off" : "on") + ".png')";
+			stateDesc.innerHTML = result.isDisabled ? "disabled" : "enabled";
+			stateDesc.style.color = result.isDisabled ? "#7C1D1D" : "#217A3D";
+			hideWikiaCheck.checked = result.hideWikia;
 		});
 	}
 	update();
