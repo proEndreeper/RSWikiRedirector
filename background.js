966c295d0ed6025d30a301a505f0e62fb53994c1
proEndreeper <devon.schneider+gpg@outlook.com>
2019-02-23 13:14:49 -0700

    Fixed issue with cached results not redirecting to new wiki.
    Extension now redirects before navigation requests instead of before web requests.
    
    Signed-off-by: proEndreeper <devon.schneider+gpg@outlook.com>

--- background.js
+++ background.js
@@ -14,7 +14,7 @@
 
   function splitURL(url){
     return RSWIKIA_REGEX.exec(url);
-  }
+  }/*
 
   chrome.webRequest.onBeforeRequest.addListener(
     function(info) {
@@ -30,7 +30,7 @@
       // Generate new url
       let redirectUrl = "https://"+newDomain+splitUrl[4].replace(/^\/wiki\//i,"/w/");
       console.log("RSWikia intercepted: " + info.url + "\nRedirecting to "+redirectUrl);
-      // Redirect the old wikia request to new wiki	
+      // Redirect the old wikia request to new wiki 
       return {redirectUrl:redirectUrl};
     },
     // filters
@@ -43,7 +43,28 @@
       ]
     },
     // extraInfoSpec
-    ["blocking"]);
+    ["blocking"]);*/
+
+  chrome.webNavigation.onBeforeNavigate.addListener( function (details) {
+    var tabId = details.tabId;
+    var tabUrl = details.url;
+    if(RSWIKIA_REGEX.test(tabUrl)) {
+      let splitUrl = splitURL(tabUrl);
+      let newDomain = REDIRECTS[splitUrl[2]];
+      // If domain isn't subdomain of wikia.com, ignore, also if it's not in the redirect filter
+      if(splitUrl===null || newDomain===undefined) return;
+      // Check if plugin is disabled, if so put a message logged
+      if(isPluginDisabled) {
+        console.log("RSWikia intercepted, ignoring because plugin is disabled.");
+        return;
+      }
+      // Generate new url
+      let redirectUrl = "https://"+newDomain+splitUrl[4].replace(/^\/wiki\//i,"/w/");
+      console.log("RSWikia intercepted: " + tabUrl + "\nRedirecting to "+redirectUrl);
+      // Redirect the old wikia request to new wiki 
+      chrome.tabs.update(tabId,{url:redirectUrl});
+    }
+})
 
   function updateIcon(){
     chrome.browserAction.setIcon({  path: isPluginDisabled?"icon32_black.png":"icon32.png"  });
