a0e381a22dd9814c3143f6b75c1cce7a8967da71
proEndreeper <devon.schneider+gpg@outlook.com>
2019-03-04 09:08:39 -0700

    Added comments regarding how the extension works.
    Signed-off-by: proEndreeper <devon.schneider+gpg@outlook.com>

--- background.js
+++ background.js
@@ -1,41 +1,46 @@
 // Simple extension to redirect all requests to RS Wikia to RS Wiki
 (function(){
   'use strict';
-  let isPluginDisabled = false;
-  let storage = window.storage || chrome.storage;
+  let isPluginDisabled = false; // Variable storing whether or not the plugin is disabled.
+  let storage = window.storage || chrome.storage; // Make sure we have a storage API.
 
-  const RSWIKIA_REGEX = /^(runescape|oldschoolrunescape)\.(wikia|fandom)\.com$/i;
+  const RSWIKIA_REGEX = /^(runescape|oldschoolrunescape)\.(wikia|fandom)\.com$/i; // Used to match the domain of the old wikia/fandom to make sure we are redirecting the correct domain.
 
+  // Listen to before anytime the browser attempts to navigate to the old Wikia/Fandom sites.
   chrome.webNavigation.onBeforeNavigate.addListener(
     function(info) {
-      if(isPluginDisabled) {
+      if(isPluginDisabled) { // Ignore all navigation requests when the extension is disabled.
         console.log("RSWikia intercepted, ignoring because plugin is disabled.");
         return;
       }
 
+      // Create a native URL object to more easily determine the path of the url and the domain.
       const url = new URL(info.url);
 
-      const isWikia = RSWIKIA_REGEX.test(url.host); // is this necessary? we already filter by URL
+      const isWikia = RSWIKIA_REGEX.test(url.host); // Check to ensure the redirect is occurring on either the fandom/wikia domain.
       // If domain isn't subdomain of wikia.com, ignore, also if it's not in the redirect filter
       if (!isWikia) return;
 
       // Generate new url
-      const host = url.host.includes('oldschool') ? 'oldschool.runescape' : 'runescape'
-      const redirectUrl = `https://${host}.wiki${url.pathname.replace(/^\/wiki\//i,"/w/")}`;
-      console.log(`RSWikia intercepted:  ${info.url}\nRedirecting to ${redirectUrl}`);
+      const host = url.host.includes('oldschool') ? 'oldschool.runescape' : 'runescape'; // Determine what host to change to, if oldschool, sub domain is oldschool.runescape, otherwise is juist runescape
+      const redirectUrl = `https://${host}.wiki${url.pathname.replace(/^\/wiki\//i,"/w/")}`; // Create the redirect URL
+      console.log(`RSWikia intercepted:  ${info.url}\nRedirecting to ${redirectUrl}`); 
       // Redirect the old wikia request to new wiki
       chrome.tabs.update(info.tabId,{url:redirectUrl});
     });
 
   function updateIcon(){
+    // Change the icon to match the state of the plugin.
     chrome.browserAction.setIcon({ path: isPluginDisabled?"icon32_black.png":"icon32.png"  });
   }
 
   storage.local.get(['isDisabled'],(result)=>{
+      // Get the initial condition of whether or not the extension is disabled
       isPluginDisabled= result ? result.isDisabled : false;
-      updateIcon();
+      updateIcon(); // Update icon to match new state
   });
 
+  // Anytime the state of the plugin changes, update the internal state of the background script.
   storage.onChanged.addListener(
       function(changes, areaName) {
         // If isDisabled changed, update isPluginDisabled
--- searchFilter.js
+++ searchFilter.js
@@ -1,27 +1,33 @@
 (function() {
   const storage = window.storage || chrome.storage;
 
+  // Check if hiding wikia results is enabled.
   storage.local.get(['hideWikia'], (result) => {
     if (result && result.hideWikia) {
+      // If hiding wikia results is enabled, grab all the links going to the old fandom and wikia sites
       const wikiaLinks = document.querySelectorAll('[href*="runescape.fandom"], [href*="runescape.wikia"]');
 
       // recursively go up through tree until getting relevant div to remove
       function getParent(element, maxDepth = 10) {
         if (element.parentElement) {
           if (element.className === 'g') {
+            // Add an element in place of the removed search result signifying a result was removed.
             var removedElement = document.createElement("span");
             removedElement.classList.add("st");
             removedElement.innerHTML="RS Wikia search result removed by RS Wiki Redirector."
             removedElement.style.paddingBottom="1em";
             removedElement.style.display="inline-block";
             element.parentElement.appendChild(removedElement);
+            //remove the element.
             element.remove();
           } else {
+            // Keep going up the elements until the necessary element is found.
             getParent(element.parentElement, maxDepth - 1);
           }
         }
       }
 
+      // Go through and process each wikia link, removing their result from the search results
       wikiaLinks.forEach((e) => getParent(e));
     }
   });
